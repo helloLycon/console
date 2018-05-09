952e7ddb4811cee198cf14d767617788c9c83d01
lk361115629 <lycon123@foxmail.com>
2018-05-09 11:49:22 +0800

    fix level-file / remove call of console.log in test.c

--- test.c
+++ test.c
@@ -4,9 +4,9 @@
 
 int main(int argc, char**argv){
     int i;
-    console.init("./log", "/tmp/lvl");
+    console.init("./log", "/tmp/console");
     for(i=0;;i++){
-        printf("write level-file(for exam., echo 7 > /tmp/lvl) then enter:");
+        printf("write level-file(for exam., echo 7 > /tmp/console) then enter:");
         fgetc(stdin);
         /* print on terminal */
         console.print(LOG_EMERG  "emergency message  (%d)\r\n", 0);
@@ -19,8 +19,7 @@ int main(int argc, char**argv){
         console.print(LOG_DEBUG  "debug message      (%d)\r\n", 7);
         console.print(           "message...         (unknown)\r\n");
         /* log into file */
-        console.log  (__FILE__,__LINE__,"LOG-SEQ  = %d\r\n", i);
-        console.vlog (                  "VLOG-SEQ = %d\r\n", i);
+        console.vlog ("VLOG-SEQ = %d\r\n", i);
     }
     return 0;
 }
